2b7542e86bc5d729490f2b7a3492de436cc04e12
You <jinjin.you@baozun.com>
2021-10-21 17:04:08 +0800

    新建组件

---
+++ src/components/FormWrap/index.js
@@ -0,0 +1,11 @@
+import styles from './index.module.scss'
+
+const FormWrap = (props) => {
+	return (
+		<div className={styles['formwrap-wrap']}>
+			<div>FormWrap</div>
+		</div>
+	)
+}
+
+export default FormWrap
